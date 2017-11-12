4574710fc89d688e4e72d2acd9c6dc9ca7353d54
Sander van den Hoven <sandervd@microsoft.com>
2017-11-12 14:57:27 +0100

    Change

--- addtoqueue.js
+++ addtoqueue.js
@@ -1,7 +1,7 @@
 var azure = require('azure-storage');
 var uuencode = require('uuencode');
 
-var queueSvc = azure.createQueueService('mindparkwebjobs', 'D6BZdyaZq+kGMxq5H1oAXy9rMElxmcQFMy581uh8lbtDrt3cDEiiOzLoogQu8y6uO2WWixXQbVojURIJKCi3TA==');
+var queueSvc = azure.createQueueService('mindparkwebjobs', '');
 
 
 queueSvc.createMessage('raspberry', 'ew0KICAiZGV2aWNlIjogIkFsbCIsDQogICJzdGF0ZSI6ICJvZmYiDQp9', function(error, result, response){
